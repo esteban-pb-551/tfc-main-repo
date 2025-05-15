61921ebe0b485b2ca256095c7afa4affea268e02
Esteban Pérez <estebanpbuday@gmail.com>
2025-05-15 11:13:55 +0000

    Add environment tagging policies and refactor EC2 configuration

--- checkov-workshop/code/terraform/simple_ec2.tf
+++ checkov-workshop/code/terraform/simple_ec2.tf
@@ -1,18 +1,17 @@
 provider "aws" {
   region = "us-west-2"
+  default_tags {
+    tags = {
+      environment = "production"
+      owner       = "Ops"
+      version     = "1.0.0"
+    }
+  }
 }
 
 resource "aws_ec2_host" "test" {
   instance_type     = "t3.micro"
   availability_zone = "us-west-2a"
-
-  tags = {
-    Environment = "production"
-  }
-}
-
-resource "aws_default" "default" {
-  // ...existing configuration...
 
   tags = {
     Environment = "production"
